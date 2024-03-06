c5ab2bd416b1de62f175bf03461c161d42a86514
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-03-06 21:25:28 +0300

    Controllers done

--- lab1/REST/Controllers/v1_0/MarkerController.cs
+++ lab1/REST/Controllers/v1_0/MarkerController.cs
@@ -1,13 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using REST.Entity.Db;
+using REST.Entity.DTO.RequestTO;
+using REST.Service.Interface;
 
 namespace REST.Controllers.V1_0
 {
-    [Route("/api/v1.0/markers")]
-    public class MarkerController(ILogger<MarkerController> Logger) : Controller
+    [Route("api/v1.0/markers")]
+    [ApiController]
+    public class MarkerController(ILogger<MarkerController> Logger, IMarkerService MarkerService) : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public JsonResult Read()
         {
-            return View();
+            var authors = MarkerService.GetAll();
+
+            Logger.LogInformation("Markers read");
+
+            return Json(authors);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] MarkerRequestTO marker)
+        {
+            var res = MarkerService.Add(marker);
+
+            Logger.LogInformation("Creating {res}", Json(marker).Value);
+
+            return await res ? Created() : BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] MarkerRequestTO marker)
+        {
+            var res = MarkerService.Update(marker);
+
+            Logger.LogInformation("Updated author: {author}", Json(marker).Value);
+
+            return await res ? Ok() : BadRequest();
+        }
+
+        [HttpPatch]
+        [Route("{id:int}")]
+        public async Task<IActionResult> PartialUpdate([FromRoute] int id, [FromBody] JsonPatchDocument<Marker> marker)
+        {
+            var res = MarkerService.Patch(id, marker);
+
+            Logger.LogInformation("Patched {author}", marker);
+
+            return await res ? Ok() : BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var res = MarkerService.Remove(id);
+
+            Logger.LogInformation("Deleted marker {id}", id);
+
+            return await res ? Ok() : BadRequest();
         }
     }
 }
--- lab1/REST/Controllers/v1_0/TweetController.cs
+++ lab1/REST/Controllers/v1_0/TweetController.cs
@@ -1,13 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using REST.Entity.Db;
+using REST.Entity.DTO.RequestTO;
+using REST.Service.Interface;
 
 namespace REST.Controllers.V1_0
 {
-    [Route("/api/v1.0/tweets")]
-    public class TweetController(ILogger<TweetController> Logger) : Controller
+    [Route("api/v1.0/tweets")]
+    [ApiController]
+    public class TweetController(ILogger<TweetController> Logger, ITweetService TweetService) : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public JsonResult Read()
         {
-            return View();
+            var authors = TweetService.GetAll();
+
+            Logger.LogInformation("Tweets read");
+
+            return Json(authors);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TweetRequestTO tweet)
+        {
+            var res = TweetService.Add(tweet);
+
+            Logger.LogInformation("Creating {res}", Json(tweet).Value);
+
+            return await res ? Created() : BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] TweetRequestTO tweet)
+        {
+            var res = TweetService.Update(tweet);
+
+            Logger.LogInformation("Updated tweet: {tweet}", Json(tweet).Value);
+
+            return await res ? Ok() : BadRequest();
+        }
+
+        [HttpPatch]
+        [Route("{id:int}")]
+        public async Task<IActionResult> PartialUpdate([FromRoute] int id, [FromBody] JsonPatchDocument<Tweet> tweet)
+        {
+            var res = TweetService.Patch(id, tweet);
+
+            Logger.LogInformation("Patched {post}", tweet);
+
+            return await res ? Ok() : BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var res = TweetService.Remove(id);
+
+            Logger.LogInformation("Deleted tweet {id}", id);
+
+            return await res ? Ok() : BadRequest();
         }
     }
 }
