4af7eb97add71c936048a8d4dc2fc95a4fd6e74a
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-03-10 20:45:38 +0300

    Working with Postgres

--- lab1/REST/Storage/SqlDb/PostgresDbContext.cs
+++ lab1/REST/Storage/SqlDb/PostgresDbContext.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using REST.Storage.Common;
 
-namespace REST.Storage.NoSqlDb
+namespace REST.Storage.SqlDb
 {
     public class PostgresDbContext : DbStorage
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+
         }
     }
 }
