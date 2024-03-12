b9e719ac8ad5b3609d8571b098794fe1742b1a08
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-03-12 16:13:15 +0300

    moved to docker

--- sqldb/REST/Storage/SqlDb/PostgresDbContext.cs
+++ sqldb/REST/Storage/SqlDb/PostgresDbContext.cs
@@ -7,7 +7,7 @@ namespace REST.Storage.SqlDb
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=distcomp;Username=postgres;Password=postgres");
+            optionsBuilder.UseNpgsql("Host=postgres;Port=5432;Database=distcomp;Username=postgres;Password=postgres");
         }
     }
 }
